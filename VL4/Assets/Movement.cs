4cc04adfa4f2a4c8834ddd645f8a74cc97ac4284
Alexander Scheurer <mail@aspepex.net>
2013-10-23 12:46:51 +0200

    VL4

---
+++ VL4/Assets/Movement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	transform.Translate(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+	transform.Rotate(Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0);
+	}
+}
