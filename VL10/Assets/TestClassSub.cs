66bb0ae98d436ad1b7becf2e64c51df73273e216
Alexander Scheurer <mail@aspepex.net>
2013-12-04 12:39:45 +0100

    VL10

---
+++ VL10/Assets/TestClassSub.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestClassSub : TestClassSuper {
+
+	public void Update()
+	{
+		base.Update();
+		transform.Rotate(rotation, 0, 0);
+	}
+}
---
+++ VL10/Assets/TestClassSuper.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestClassSuper : MonoBehaviour {
+	
+	protected float rotation;
+	
+	public void Start()
+	{
+		rotation = 5.0f;
+	}
+		
+	public void Update()
+	{
+		transform.Rotate(0,rotation,0);
+	}
+}
