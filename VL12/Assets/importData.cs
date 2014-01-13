613030d92a5bcaf4f61ad9accbd0abb837dece37
Alexander Scheurer <alexander.scheurer@hs-furtwangen.de>
2014-01-13 11:18:28 +0100

    VL12 - a little late

---
+++ VL12/Assets/importData.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class importData : MonoBehaviour
+{
+	
+	private string[] aExternalData = {};
+		
+	// Use this for initialization
+	void Start ()
+	{
+	
+		string filename = "ExternalData/ExternalData.txt";
+		string iniData = "";
+		string prefix = "";
+
+		if (!Application.isWebPlayer)
+		{
+			prefix = "file://";
+			Debug.Log("not a Webplayer");
+		}
+		var pathname = prefix + Application.dataPath + "/../" + filename;
+		Debug.Log (pathname);
+		
+		StartCoroutine (DoWWW (pathname));
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+	}
+
+	private IEnumerator DoWWW (string path)
+	{
+
+		WWW www = new WWW (path);
+		yield return www;
+
+		print (www.text);
+
+	}
+}
+
