ae1b8786a79440f115be312d1329ac7b6766167d
Alexander Scheurer <alexander.scheurer@hs-furtwangen.de>
2014-01-15 12:36:51 +0100

    VL13

---
+++ VL13/Assets/MessageRelay.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MessageRelay : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void RelayMessage(string bla)
+	{
+		BroadcastMessage(bla);
+	}
+
+}
