484e4548784d50c3b85a909eaa098352e71782ff
Alexander Scheurer <mail@aspepex.net>
2014-02-17 20:44:27 +0100

    CameraShake Script

---
+++ Fu-Type_U/Fu-Type_U/Assets/CameraShake.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+// ReSharper disable once CheckNamespace
+public class CameraShake : MonoBehaviour
+{
+    private Vector3 _originPosition;
+    private Quaternion _originRotation;
+    private float _shakeDecay;
+    private float _shakeIntensity;
+
+    // ReSharper disable once UnusedMember.Local
+    private void Start()
+    {
+    }
+
+    // ReSharper disable once UnusedMember.Local
+    private void Update()
+    {
+        if (_shakeIntensity > 0)
+        {
+            transform.position = _originPosition + Random.insideUnitSphere*_shakeIntensity;
+            transform.rotation = new Quaternion(
+                _originRotation.x + Random.Range(-_shakeIntensity, _shakeIntensity)*0.1f,
+                _originRotation.y + Random.Range(-_shakeIntensity, _shakeIntensity)*0.1f,
+                _originRotation.z + Random.Range(-_shakeIntensity, _shakeIntensity)*0.1f,
+                _originRotation.w + Random.Range(-_shakeIntensity, _shakeIntensity)*0.1f);
+            _shakeIntensity -= _shakeDecay;
+        }
+    }
+
+
+    public void Shake()
+    {
+        _originPosition = transform.position;
+        _originRotation = transform.rotation;
+        _shakeIntensity = 0.2f;
+        _shakeDecay = 0.005f;
+    }
+}
